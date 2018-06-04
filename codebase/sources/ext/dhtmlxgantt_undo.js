5d518e4bb624e15bec7942c6fd9db900469bdd09
Frank Siler <franklin.siler@equifax.com>
2018-06-04 14:39:02 -0500

    whitespace

--- codebase/sources/ext/dhtmlxgantt_undo.js
+++ codebase/sources/ext/dhtmlxgantt_undo.js
@@ -1,11 +1,11 @@
 /*!
  * @license
- * 
+ *
  * dhtmlxGantt v.5.1.0 Stardard
  * This software is covered by GPL license. You also can obtain Commercial or Enterprise license to use it in non-GPL project - please contact sales@dhtmlx.com. Usage without proper license is prohibited.
- * 
+ *
  * (c) Dinamenta, UAB.
- * 
+ *
  */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
@@ -89,19 +89,19 @@ gantt.config.undo_steps = 10;
 gantt.config.undo = true;
 gantt.config.redo = true;
 
-gantt.undo = function(){
+gantt.undo = function() {
 	this._undo.undo();
 };
 
-gantt.getUndoStack = function(){
+gantt.getUndoStack = function() {
 	return this._undo._undoStack;
 };
 
-gantt.getRedoStack = function(){
+gantt.getRedoStack = function() {
 	return this._undo._redoStack;
 };
 
-gantt.redo = function(){
+gantt.redo = function() {
 	this._undo.redo();
 };
 
@@ -111,7 +111,7 @@ gantt.redo = function(){
  */
 gantt.config.undo_types = {
 	link: "link",
-	task: "task"
+	task: "task",
 };
 
 /**
@@ -121,38 +121,39 @@ gantt.config.undo_types = {
 gantt.config.undo_actions = {
 	update:"update",
 	remove:"remove",// remove item from datastore
+        move: "move",
 	add:"add"
 };
 
 gantt._undo = {
-	_undoStack:[],
-	_redoStack:[],
-	maxSteps:10,
+	_undoStack: [],
+	_redoStack: [],
+	maxSteps: 10,
 	undo_enabled: true,
 	redo_enabled: true,
-	_push: function(stack, action){
+	_push: function(stack, action) {
 		if(!action.commands.length)
 			return;
 
 
 		stack.push(action);
-		while(stack.length > this.maxSteps){
+		while(stack.length > this.maxSteps) {
 			stack.shift();
 		}
 		return action;
 	},
-	_pop:function(stack){
+	_pop:function(stack) {
 		return stack.pop();
 	},
 
-	undo:function(){
+	undo:function() {
 		this.updateConfigs();
 		if(!this.undo_enabled)
 			return;
 
 		var action = this._pop(this._undoStack);
-		if(gantt.callEvent("onBeforeUndo", [action]) !== false){
-			if(action){
+		if(gantt.callEvent("onBeforeUndo", [action]) !== false) {
+			if(action) {
 
 				this._applyAction(this.action.invert(action));
 				this._push(this._redoStack, gantt.copy(action));
@@ -161,14 +162,14 @@ gantt._undo = {
 		gantt.callEvent("onAfterUndo", []);
 	},
 
-	redo:function(){
+	redo:function() {
 		this.updateConfigs();
 		if(!this.redo_enabled)
 			return;
 
 		var action = this._pop(this._redoStack);
-			if(gantt.callEvent("onBeforeRedo", [action]) !== false){
-				if(action){
+			if(gantt.callEvent("onBeforeRedo", [action]) !== false) {
+				if(action) {
 					this._applyAction(action);
 					this._push(this._undoStack, gantt.copy(action));
 				}
@@ -176,7 +177,7 @@ gantt._undo = {
 		gantt.callEvent("onAfterRedo", []);
 	},
 
-	_applyAction:function(action){
+	_applyAction:function(action) {
 		var command = null,
 			entities = this.command.entity,
 			actions = this.command.type;
@@ -186,6 +187,7 @@ gantt._undo = {
 			add: "addTask",
 			update: "updateTask",
 			remove: "deleteTask",
+                        move: "onAfterTaskMove",
 			isExists: "isTaskExists"
 		};
 		methods[entities.link] = {
@@ -194,19 +196,21 @@ gantt._undo = {
 			remove: "deleteLink",
 			isExists: "isLinkExists"
 		};
+                methods[entities.move] = {
+                }
 
-		gantt.batchUpdate(function(){
-			for(var i = 0; i < action.commands.length; i++){
+		gantt.batchUpdate(function() {
+			for(var i = 0; i < action.commands.length; i++) {
 				command = action.commands[i];
 				var method = methods[command.entity][command.type],
 					check = methods[command.entity]["isExists"];
 
-				if(command.type == actions.add){
+				if(command.type == actions.add) {
 					gantt[method](command.oldValue, command.oldValue.parent, command.oldValue.$index);
-				}else if(command.type == actions.remove){
+				}else if(command.type == actions.remove) {
 					if(gantt[check](command.value.id))
 						gantt[method](command.value.id);
-				}else if(command.type == actions.update){
+				}else if(command.type == actions.update) {
 
 					gantt[method](command.value.id, command.value);
 				}
@@ -215,22 +219,22 @@ gantt._undo = {
 	},
 
 	//storeUndo:
-	logAction: function(action){
+	logAction: function(action) {
 		this._push(this._undoStack, action);
 		this._redoStack = [];
 	},
 
 	action: {
-		create: function(commands){
+		create: function(commands) {
 			return {commands: commands ? commands.slice() : []};
 		},
-		invert: function(action){
+		invert: function(action) {
 			var revert = gantt.copy(action);
 			var commands = gantt._undo.command;
 
-			for(var i = 0; i < action.commands.length; i++){
+			for(var i = 0; i < action.commands.length; i++) {
 				var command = revert.commands[i] = commands.invert(revert.commands[i]);
-				if(command.type == commands.type.update){
+				if(command.type == commands.type.update) {
 					var value = command.value;
 					command.value = command.oldValue;
 					command.oldValue = value;
@@ -241,7 +245,7 @@ gantt._undo = {
 	},
 
 	command:{
-		create: function(value, oldValue, type, entity){
+		create: function(value, oldValue, type, entity) {
 			return {
 				entity:entity,
 				type:type,
@@ -249,7 +253,7 @@ gantt._undo = {
 				oldValue:gantt.copy(oldValue || value)
 			};
 		},
-		invert: function(command){
+		invert: function(command) {
 			var revert = gantt.copy(command);
 			revert.type = this.inverseCommands(command.type);
 			return revert;
@@ -259,8 +263,8 @@ gantt._undo = {
 
 		//types of traced actions
 		type:null,
-		inverseCommands:function(command){
-			switch (command){
+		inverseCommands:function(command) {
+			switch (command) {
 				case this.type.update:
 					return this.type.update;
 				case this.type.remove:
@@ -282,18 +286,18 @@ gantt._undo = {
 		_batchAction: null,
 		_batchMode: false,
 		_ignore: false,
-		startIgnore: function(){
+		startIgnore: function() {
 			this._ignore = true;
 		},
-		stopIgnore: function(){
+		stopIgnore: function() {
 			this._ignore = false;
 		},
-		startBatchAction : function(){
+		startBatchAction: function() {
 			// try catching updates made from event handlers using timeout
-			if(this.timeout){
+			if(this.timeout) {
 				clearTimeout(this.timeout);
 			}
-			this.timeout = setTimeout(function(){
+			this.timeout = setTimeout(function() {
 				gantt._undo.monitor.stopBatchAction();
 			}, 10);
 
@@ -304,94 +308,94 @@ gantt._undo = {
 			this._batchMode = true;
 			this._batchAction = gantt._undo.action.create();
 		},
-		stopBatchAction: function(){
+		stopBatchAction: function() {
 			if(this._ignore)
 				return;
 			var undo = gantt._undo;
-			if(this._batchAction){
+			if(this._batchAction) {
 				undo.logAction(this._batchAction);
 			}
 			this._batchMode = false;
 			this._batchAction = null;
 		},
 
-		_storeCommand: function(command){
+		_storeCommand: function(command) {
 			var undo = gantt._undo;
 			undo.updateConfigs();
 
 			if(!undo.undo_enabled)
 				return;
 
-			if(this._batchMode){
+			if(this._batchMode) {
 				this._batchAction.commands.push(command);
 			}else{
 				var action = undo.action.create([command]);
 				undo.logAction(action);
 			}
 		},
-		_storeEntityCommand: function(obj, old, actionType, entityType){
+		_storeEntityCommand: function(obj, old, actionType, entityType) {
 			var undo = gantt._undo;
 			var command = undo.command.create(obj, old, actionType, entityType);
 			this._storeCommand(command);
 		},
-		_storeTaskCommand: function(obj, type){
+		_storeTaskCommand: function(obj, type) {
 			this._storeEntityCommand(obj, this.getInitialTask(obj.id), type, gantt._undo.command.entity.task);
 		},
-		_storeLinkCommand: function(obj, type){
+		_storeLinkCommand: function(obj, type) {
 			this._storeEntityCommand(obj, this.getInitialLink(obj.id), type, gantt._undo.command.entity.link);
 		},
 
 
-		onTaskAdded:function(task){
+		onTaskAdded: function(task) {
 			if(!this._ignore)
 				this._storeTaskCommand(task, gantt._undo.command.type.add);
 		},
-		onTaskUpdated:function(task){
+		onTaskUpdated: function(task) {
 			if(!this._ignore)
 				this._storeTaskCommand(task, gantt._undo.command.type.update);
 		},
-		onTaskDeleted: function(task){
-			if(!this._ignore){
+		onTaskDeleted: function(task) {
+			if(!this._ignore) {
 				this._storeTaskCommand(task, gantt._undo.command.type.remove);
-				if(this._nestedTasks[task.id]){
+				if(this._nestedTasks[task.id]) {
 					var children = this._nestedTasks[task.id];
-					for(var i = 0; i < children.length; i++){
+					for(var i = 0; i < children.length; i++) {
 						this._storeTaskCommand(children[i], gantt._undo.command.type.remove);
 					}
 				}
-				if(this._nestedLinks[task.id]){
+				if(this._nestedLinks[task.id]) {
 					var childrenLinks = this._nestedLinks[task.id];
-					for(var i = 0; i < childrenLinks.length; i++){
+					for(var i = 0; i < childrenLinks.length; i++) {
 						this._storeLinkCommand(childrenLinks[i], gantt._undo.command.type.remove);
 					}
 				}
 			}
 		},
 
-		onLinkAdded: function(link){
+		onLinkAdded: function(link) {
 			if(!this._ignore)
 				this._storeLinkCommand(link, gantt._undo.command.type.add);
 		},
-		onLinkUpdated: function(link){
+		onLinkUpdated: function(link) {
 			if(!this._ignore)
 				this._storeLinkCommand(link, gantt._undo.command.type.update);
 		},
-		onLinkDeleted: function(link){
+		onLinkDeleted: function(link) {
 			if(!this._ignore)
 				this._storeLinkCommand(link, gantt._undo.command.type.remove);
 		},
 		_initialTasks:{},
 		_nestedTasks:{},
 		_nestedLinks:{},
-		_getLinks: function(task){
+		_getLinks: function(task) {
 			return task.$source.concat(task.$target);
 		},
-		setNestedTasks: function(id, taskIds){
+		setNestedTasks: function(id, taskIds) {
 			var task = null,
 				tasks = [],
 				linkIds = this._getLinks(gantt.getTask(id));
 
-			for(var i = 0; i < taskIds.length; i++){
+			for(var i = 0; i < taskIds.length; i++) {
 				task = this.setInitialTask(taskIds[i]);
 
 				linkIds = linkIds.concat(this._getLinks(task));
@@ -401,18 +405,18 @@ gantt._undo = {
 			}
 
 			var uniqueLinks = {};
-			for(var i = 0; i < linkIds.length; i++){
+			for(var i = 0; i < linkIds.length; i++) {
 				uniqueLinks[linkIds[i]] = true;
 			}
 			var links = [];
-			for(var i in uniqueLinks){
+			for(var i in uniqueLinks) {
 				links.push(this.setInitialLink(i));
 			}
 			this._nestedTasks[id] = tasks;
 			this._nestedLinks[id] = links;
 		},
-		setInitialTask: function(id){
-			if(!this._initialTasks[id] || !this._batchMode){
+		setInitialTask: function(id) {
+			if(!this._initialTasks[id] || !this._batchMode) {
 				var task = gantt.copy( gantt.getTask(id));
 				task.$index = gantt.getTaskIndex(id);
 
@@ -420,25 +424,25 @@ gantt._undo = {
 			}
 			return this._initialTasks[id];
 		},
-		getInitialTask: function(id){
+		getInitialTask: function(id) {
 			return this._initialTasks[id];
 		},
 
-		_initialLinks:{},
-		setInitialLink: function(id){
+		_initialLinks: {},
+		setInitialLink: function(id) {
 			if(!this._initialLinks[id] || !this._batchMode)
 				this._initialLinks[id] = gantt.copy(gantt.getLink(id));
 
 			return this._initialLinks[id];
 		},
-		getInitialLink: function(id){
+		getInitialLink: function(id) {
 			return this._initialLinks[id];
 		}
 	}
 
 
 };
-gantt._undo.updateConfigs = function(){
+gantt._undo.updateConfigs = function() {
 	gantt._undo.maxSteps = gantt.config.undo_steps;
 	gantt._undo.command.entity = gantt.config.undo_types;
 	gantt._undo.command.type = gantt.config.undo_actions;
@@ -447,19 +451,18 @@ gantt._undo.updateConfigs = function(){
 };
 
 
-(function(){
-
+(function() {
 	var monitor = gantt._undo.monitor;
 
 	var noTrack = {
 		"onBeforeUndo":"onAfterUndo",
 		"onBeforeRedo": "onAfterRedo"
 	};
-	for(var i in noTrack){
-		gantt.attachEvent(i, function(){
+	for(var i in noTrack) {
+		gantt.attachEvent(i, function() {
 			monitor.startIgnore();
 		});
-		gantt.attachEvent(noTrack[i], function(){
+		gantt.attachEvent(noTrack[i], function() {
 			monitor.stopIgnore();
 		});
 	}
@@ -470,79 +473,77 @@ gantt._undo.updateConfigs = function(){
 		"onBeforeBatchUpdate"
 	];
 
-	for(var i  = 0; i < batchActions.length; i++){
-		gantt.attachEvent(batchActions[i], function(){
+	for(var i  = 0; i < batchActions.length; i++) {
+		gantt.attachEvent(batchActions[i], function() {
 			monitor.startBatchAction();
 		});
 	}
-	function store(id){
+	function store(id) {
 		monitor.setInitialTask(id);
 		return true;
 	}
 
-
-
 	gantt.attachEvent("onBeforeTaskDrag", store);
 	gantt.attachEvent("onLightbox", store);
-	gantt.attachEvent("onBeforeTaskAutoSchedule", function(task){ store(task.id);  return true;});
-	gantt.attachEvent("onBeforeTaskDelete", function(id){
+	gantt.attachEvent("onBeforeTaskAutoSchedule", function(task) { store(task.id);  return true;});
+	gantt.attachEvent("onBeforeTaskDelete", function(id) {
 		store(id);
 		var nested = [];
-		gantt.eachTask(function(task){
+		gantt.eachTask(function(task) {
 			nested.push(task.id);
 		}, id);
 		monitor.setNestedTasks(id, nested);
 		return true;
 	});
 
-	gantt.attachEvent("onAfterTaskAdd", function(id, task){
+	gantt.attachEvent("onAfterTaskAdd", function(id, task) {
 		monitor.onTaskAdded(task);
 	});
-	gantt.attachEvent("onAfterTaskUpdate", function(id, task){
+	gantt.attachEvent("onAfterTaskUpdate", function(id, task) {
 		monitor.onTaskUpdated(task);
 	});
-	gantt.attachEvent("onAfterTaskDelete", function(id, task){
+	gantt.attachEvent("onAfterTaskDelete", function(id, task) {
 		monitor.onTaskDeleted(task);
 	});
 
-	gantt.attachEvent("onAfterLinkAdd", function(id, task){
+	gantt.attachEvent("onAfterLinkAdd", function(id, task) {
 		monitor.onLinkAdded(task);
 	});
-	gantt.attachEvent("onAfterLinkUpdate", function(id, task){
+	gantt.attachEvent("onAfterLinkUpdate", function(id, task) {
 		monitor.onLinkUpdated(task);
 	});
-	gantt.attachEvent("onAfterLinkDelete", function(id, task){
+	gantt.attachEvent("onAfterLinkDelete", function(id, task) {
 		monitor.onLinkDeleted(task);
 	});
 
-	function updTask(task, oldId, newId){
+	function updTask(task, oldId, newId) {
 		if(!task) return;
 
-		if(task.id == oldId){
+		if(task.id == oldId) {
 			task.id = newId;
 		}
 
-		if(task.parent == oldId){
+		if(task.parent == oldId) {
 			task.parent = newId;
 		}
 	}
 
-	function changeTaskCommandId(command, oldId, newId){
+	function changeTaskCommandId(command, oldId, newId) {
 		updTask(command.value, oldId, newId);
 		updTask(command.oldValue, oldId, newId);
 	}
 
-	function updLink(link, oldTaskId, newTaskId){
+	function updLink(link, oldTaskId, newTaskId) {
 		if(!link) return;
-		if(link.source == oldTaskId){
+		if(link.source == oldTaskId) {
 			link.source = newTaskId;
 		}
-		if(link.target == oldTaskId){
+		if(link.target == oldTaskId) {
 			link.target = newTaskId;
 		}
 	}
 
-	function changeLinkCommandId(command, oldId, newId){
+	function changeLinkCommandId(command, oldId, newId) {
 		updLink(command.value, oldId, newId);
 		updLink(command.oldValue, oldId, newId);
 	}
@@ -562,7 +563,7 @@ gantt._undo.updateConfigs = function(){
 		}
 	}
 
-	function updateLinksIds(log, oldId, newId){
+	function updateLinksIds(log, oldId, newId) {
 		var undo = gantt._undo;
 
 		for (var i = 0; i < log.length; i++) {
@@ -570,10 +571,10 @@ gantt._undo.updateConfigs = function(){
 			for (var j = 0; j < entry.commands.length; j++) {
 				var command = entry.commands[j];
 				if (command.entity == undo.command.entity.link) {
-					if(command.value && command.value.id == oldId){
+					if(command.value && command.value.id == oldId) {
 						command.value.id = newId;
 					}
-					if(command.oldValue && command.oldValue.id == oldId){
+					if(command.oldValue && command.oldValue.id == oldId) {
 						command.oldValue.id = newId;
 					}
 				}
@@ -581,17 +582,17 @@ gantt._undo.updateConfigs = function(){
 		}
 	}
 
-	gantt.attachEvent("onTaskIdChange", function(oldId, newId){
+	gantt.attachEvent("onTaskIdChange", function(oldId, newId) {
 		var undo = gantt._undo;
 		updateTasksIds(undo._undoStack,oldId, newId);
 		updateTasksIds(undo._redoStack,oldId, newId);
 	});
-	gantt.attachEvent("onLinkIdChange", function(oldId, newId){
+	gantt.attachEvent("onLinkIdChange", function(oldId, newId) {
 		var undo = gantt._undo;
 		updateLinksIds(undo._undoStack,oldId, newId);
 		updateLinksIds(undo._redoStack,oldId, newId);
 	});
-	gantt.attachEvent("onGanttReady", function(){
+	gantt.attachEvent("onGanttReady", function() {
 		gantt._undo.updateConfigs();
 	});
 })();
